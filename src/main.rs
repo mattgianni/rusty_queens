f62504e4dc37a333a0eb17e359051c566e5583ce
Matthew Gianni <matt@gianni.org>
2024-06-14 20:15:16 -0700

    transfer commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use cfg_if::cfg_if;
 use clap::{Arg, ArgAction, Command};
 #[allow(unused_imports)]
 use rusty_queens::Board;
 
+cfg_if! {
+    if #[cfg(feature = "build")] {
+        const VERSION: &str = env!("CARGO_PKG_VERSION");
+        const GIT_COMMIT_HASH: &str = env!("VERGEN_GIT_SHA");
+        const BUILD_TIMESTAMP: &str = env!("VERGEN_BUILD_TIMESTAMP");
+    } else {
+        const VERSION: &str = "unknown";
+        const GIT_COMMIT_HASH: &str = "unknown";
+        const BUILD_TIMESTAMP: &str = "unknown";
+    }
+}
+
 fn main() {
+    let version = format!("{}-{}-{}", VERSION, GIT_COMMIT_HASH, BUILD_TIMESTAMP);
     let matches = Command::new("rusty_queens")
-        .version("0.1.0")
+        .version(version)
         .author("Matthew Gianni <matt@gianni.org>")
         .about("learning Rust by example")
         .arg(
@@ -18,5 +32,7 @@ fn main() {
 
     // Get the value of the "input" argument
     let verbose = matches.get_flag("verbose");
-    println!("Verbose flag: {}", verbose);
+    if verbose {
+        println!("Verbose flag: {}", verbose);
+    }
 }
